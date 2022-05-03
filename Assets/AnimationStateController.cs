a395d3ab5eb692313d2dbb099de99033c6083df8
Nassredean Nasseri <dean@nasseri.io>
2022-05-03 16:36:46 -0400

    Add animated mixamo avatar with idle, walk, and run animations + controller

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (!isWalking && forwardPressed) {
+            animator.SetBool("isWalking", true);
+        }
+        if (isWalking && !forwardPressed) {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (!isRunning && (forwardPressed && runPressed)) {
+            animator.SetBool("isRunning", true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed)) {
+            animator.SetBool("isRunning", false);
+        }
+
+    }
+}

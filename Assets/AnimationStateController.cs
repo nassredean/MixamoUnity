4b406469424ba82ade008c77592949f5d0afd2ac
Nassredean Nasseri <dean@nasseri.io>
2022-05-05 18:35:19 -0400

    add animation blend trees

--- Assets/AnimationStateController.cs
+++ Assets/AnimationStateController.cs
@@ -5,36 +5,42 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     Animator animator;
+    float velocity = 0.0f;
+    public float acceleration = 0.1f;
+    public float deceleration = 0.5f;
+    int VelocityHash;
 
+    
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        
+        VelocityHash = Animator.StringToHash("Blend");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = animator.GetBool("isWalking");
-        bool isRunning = animator.GetBool("isRunning");
-
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
 
-        if (!isWalking && forwardPressed) {
-            animator.SetBool("isWalking", true);
-        }
-        if (isWalking && !forwardPressed) {
-            animator.SetBool("isWalking", false);
+        if (forwardPressed && velocity < 1.0f) {
+            velocity += Time.deltaTime * acceleration;
         }
 
-        if (!isRunning && (forwardPressed && runPressed)) {
-            animator.SetBool("isRunning", true);
+        if (!forwardPressed && velocity > 0.0f) {
+            velocity -= Time.deltaTime * deceleration;
         }
 
-        if (isRunning && (!forwardPressed || !runPressed)) {
-            animator.SetBool("isRunning", false);
+        if (!forwardPressed && velocity < 0.0f) {
+            velocity = 0.0f;
         }
 
+        if (forwardPressed && velocity > 1.0f) {
+            velocity = 1.0f;
+        }
+
+        animator.SetFloat(VelocityHash, velocity);
     }
 }
